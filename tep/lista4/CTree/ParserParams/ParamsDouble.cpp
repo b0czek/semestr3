490514f8717980dfab241ca1bcca3eb78f24537a
Dariusz Majnert <dariusz.majnert@gmail.com>
2023-12-01 13:35:01 +0100

    lista 5

---
+++ tep/lista4/CTree/ParserParams/ParamsDouble.cpp
@@ -0,0 +1,3 @@
+//
+// Created by dariusz on 01.12.23.
+//
---
+++ tep/lista4/CTree/ParserParams/ParamsDouble.h
@@ -0,0 +1,8 @@
+//
+// Created by dariusz on 01.12.23.
+//
+
+#ifndef LISTA3_PARAMSDOUBLE_H
+#define LISTA3_PARAMSDOUBLE_H
+
+#endif //LISTA3_PARAMSDOUBLE_H
